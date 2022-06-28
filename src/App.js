38ebb27262c7388da263c40028e00dd5f4997ee3
эльмар <elimareg@gmail.com>
2022-06-28 18:15:35 +0300

    api & mui ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [coords, setCoords] = useState()
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => setCoords([position.coords.latitude, position.coords.longitude])
+    )
+  }, [])
+
+  useEffect(() => {
+    if (!coords) return
+    const fetchData = async () => await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${coords[0]}&lon=${coords[1]}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(res => setData(res))
+      .catch(err => {throw new Error(err)})
+    fetchData()
+  }, [coords])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
