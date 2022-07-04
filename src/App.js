281cc696cf2a778016f829dbb4f5dc2e552f1a47
эльмар <elimareg@gmail.com>
2022-07-04 16:24:46 +0300

    copy paste widget + dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
+import Weather from './components/Weather'
+import Window from './components/animation/Window'
 
 function App() {
   const [coords, setCoords] = useState()
@@ -11,17 +13,21 @@ function App() {
     )
   }, [])
 
-  useEffect(() => {
-    if (!coords) return
-    const fetchData = async () => await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${coords[0]}&lon=${coords[1]}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-      .then(res => res.json())
-      .then(res => setData(res))
-      .catch(err => {throw new Error(err)})
-    fetchData()
-  }, [coords])
+  // useEffect(() => {
+  //   if (!coords) return
+  //   const fetchData = async () => await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${coords[0]}&lon=${coords[1]}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+  //     .then(res => res.json())
+  //     .then(res => setData(res))
+  //     .catch(err => {throw new Error(err)})
+  //   fetchData()
+  // }, [coords])
 
   return (
     <div className="App">
+      {/* <Weather/> */}
+      <Window/>
+      {/* <div>no data</div> */}
+      {/* {data ? <Weather data={data}/> : <div></div>} */}
     </div>
   );
 }
