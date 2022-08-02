3646f65240cf9d0eb1080be0ea6133bc6a582ae7
эльмар <elimareg@gmail.com>
2022-08-02 21:05:11 +0300

    accordion added to hourly tab

--- src/components/weatherTabs/Hourly.js
+++ src/components/weatherTabs/Hourly.js
@@ -1,13 +1,23 @@
 import Hour from './Hour'
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import { Box } from '@mui/material';
 
 export default function Hourly ({forecast, deg}) {
-
   const currentDate = new Date()
-  
-  const hours = forecast.forecastday.slice(0, 4).map(day => <div key={day.date}>
-  <span>{new Date(day.date).toLocaleString('default', {weekday: 'long'})}</span>
-  {day.hour.map(hour => currentDate < new Date(hour.time) && <Hour key={hour.time} data={hour} deg={deg}/>)}
-  </div>)
 
-  return hours
+  const [firstD, secD, thirdD] = forecast.forecastday
+  const days = [firstD.hour.filter(hour => currentDate < new Date(hour.time)), secD.hour, thirdD.hour]
+
+  const daysAccord = days.map((day, i) => <Accordion
+  TransitionProps={{ unmountOnExit: true }}
+  defaultExpanded={i === 0}
+  key={day[i].time_epoch}
+  sx={{background: 'transparent'}}>
+    <AccordionSummary>{new Date(day[i].time).toLocaleString('default', {weekday: 'long'})}</AccordionSummary>
+    <AccordionDetails>{day.map(hour => <Hour key={hour.time} data={hour} deg={deg}/>)}</AccordionDetails>
+  </Accordion>)
+  
+  return <Box width='1'>{daysAccord}</Box>
 }
